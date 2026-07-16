b71ba3dd6704b2e54397f9b0e1f02cd6545e8025
Mahmood Virtuo <mahmood@virtuo.ae>
2026-07-16 18:23:22 +0400

    Fonts Preloading Added Above the Fold

--- partials/seo.php
+++ partials/seo.php
@@ -97,7 +97,7 @@ include_once dirname(__DIR__) . '/includes/schema.php';
 <link rel="canonical" href="<?php echo virtuo_seo_escape($canonicalUrl); ?>">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500&display=swap">
 <?php if ($heroImage !== '' && $heroImageMobile !== '') : ?>
 <?php if ($heroImage === $heroImageMobile) : ?>
 <link rel="preload" as="image" href="<?php echo virtuo_seo_escape($heroImage); ?>" fetchpriority="high">
