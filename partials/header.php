2cfe1df5e9d50974af291512f65406bf2e4d4876
Mahmood Virtuo <mahmood@virtuo.ae>
2026-07-22 21:56:47 +0400

    Fix homepage Services navigation chevron

--- partials/header.php
+++ partials/header.php
@@ -79,7 +79,7 @@ preloader disabled end -->
                             <li>
                                 <a href="/about">About</a>
                             </li>
-                            <li class="menu-item-has-children virtuo-mega-menu-trigger"><a href="#">Services</a>
+                            <li class="menu-item-has-children virtuo-mega-menu-trigger"><a href="#">Services<?php if (!empty($useInlineFontAwesomeIcons)) : ?><svg class="home-services-nav-chevron" xmlns="http://www.w3.org/2000/svg" width=".875em" height="1em" viewBox="0 0 448 512" fill="currentColor" focusable="false" aria-hidden="true"><path transform="rotate(180 224 256)" d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"/></svg><?php endif; ?></a>
                                 <div class="virtuo-services-mega" data-virtuo-mega-menu>
                                     <div class="virtuo-services-mega__inner">
                                         <div class="virtuo-services-mega__categories" role="list">
