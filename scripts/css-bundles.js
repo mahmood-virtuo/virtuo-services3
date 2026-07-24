ea4c98721b899b6a6564b7ebe98337c4c83b8ce4
Mahmood Virtuo <mahmood@virtuo.ae>
2026-07-25 01:21:31 +0400

    Revert "Add critical CSS delivery for root blog page"
    This reverts commit 30026fd7336f836f7acd0ec766e643c445715d2d.

--- scripts/css-bundles.js
+++ scripts/css-bundles.js
@@ -31,12 +31,6 @@ const bundles = [
     output: `${bundleRoot}/home-core-critical.min.css`,
     allowEmpty: false,
   },
-  {
-    name: "blog-listing-core-critical",
-    source: `${sourceRoot}/critical/blog-listing-core-critical.css`,
-    output: `${bundleRoot}/blog-listing-core-critical.min.css`,
-    allowEmpty: false,
-  },
 ];
 
 module.exports = Object.freeze({
@@ -47,7 +41,7 @@ module.exports = Object.freeze({
   compatibility: Object.freeze({
     sources: Object.freeze(
       bundles
-        .filter((bundle) => !bundle.name.endsWith("-critical"))
+        .filter((bundle) => bundle.name !== "home-core-critical")
         .map((bundle) => bundle.source)
     ),
     unminifiedOutput: "assets/css/main.css",
